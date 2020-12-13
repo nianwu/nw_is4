7fd5f59dd0ea11990036eb93ce16b7f3edffff13
nianwu <31959790+nianwu@users.noreply.github.com>
2020-12-13 11:26:07 +0800

    添加 api 和 client 配置

--- quickstart/src/IdentityServer/Config.cs
+++ quickstart/src/IdentityServer/Config.cs
@@ -11,16 +11,33 @@ namespace IdentityServer
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId()
             };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            { };
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
+        };
 
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+        public static IEnumerable<Client> Clients => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
     }
 }
