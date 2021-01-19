04fce0cfd742261e65cea5a83fb806f5b3f82310
nianwu <31959790+nianwu@users.noreply.github.com>
2021-01-19 13:22:10 +0800

    添加 user 相关接口

--- IdentityServerAspNetIdentity/Controllers/UsersController.cs
+++ IdentityServerAspNetIdentity/Controllers/UsersController.cs
@@ -1,12 +1,27 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using IdentityServerHost.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServerAspNetIdentity.Controllers
 {
+    public static class UserExtend
+    {
+        public static Expression<Func<ApplicationUser, bool>> Filter(this IQueryable<ApplicationUser> e, string keyword)
+        {
+            return x =>
+                string.IsNullOrWhiteSpace(keyword)
+                || x.Id == keyword
+                || x.UserName.Contains(keyword)
+                || x.Email.Contains(keyword)
+                || x.PhoneNumber.Contains(keyword);
+        }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
@@ -19,6 +34,15 @@ namespace IdentityServerAspNetIdentity.Controllers
         {
             this._userManager = userManager;
         }
+        private Expression<Func<ApplicationUser, bool>> DefaultFilter(string keyword)
+        {
+            return x =>
+                string.IsNullOrWhiteSpace(keyword)
+                || x.Id == keyword
+                || x.UserName.Contains(keyword)
+                || x.Email.Contains(keyword)
+                || x.PhoneNumber.Contains(keyword);
+        }
 
         [HttpGet]
         public List<ApplicationUser> Get(
@@ -28,18 +52,26 @@ namespace IdentityServerAspNetIdentity.Controllers
         )
         {
             var users = _userManager.Users
-                .Where(x =>
-                    string.IsNullOrWhiteSpace(keyword)
-                    || x.Id == keyword
-                    || x.UserName.Contains(keyword)
-                    || x.Email.Contains(keyword)
-                    || x.PhoneNumber.Contains(keyword)
-                )
+                .Where(DefaultFilter(keyword))
                 .Skip((page - 1) * limit)
                 .Take(limit)
                 .ToList();
 
             return users;
+        }
+
+        [HttpGet("[controller]")]
+        public int Count(
+            [MinLength(2)] string keyword
+        )
+        {
+            return _userManager.Users.Count(DefaultFilter(keyword));
+        }
+
+        [HttpPost]
+        public void Create(ApplicationUser user, string password)
+        {
+            _userManager.CreateAsync(user, password);
         }
     }
 }
